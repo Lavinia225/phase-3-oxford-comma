bc8e1efcb63d3daf7980a0a3666a970d449a2147
Luna Seifert <thepuddingdeity@gmail.com>
2023-04-07 14:49:52 -0500

    commit tree 0ab4f83c68af6c4e460aad1383dfdcf8927bf8c3

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if (array.length > 2)
+        last_word = array.pop
+        last_word_modified = ", and " + last_word
+        array.join(", ") << last_word_modified
+    elsif (array.length == 2)
+        array[0] + " and " + array[1]
+    else
+        array[0]
+    end
 end
